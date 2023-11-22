6b1c65ac7e71108260506484eb6509bd8d916481
CodeDok <entwicklung@codedok.de>
2023-11-23 00:52:17 +0100

    added intellij keymaps

--- keyboards/sofle_choc/keymaps/CodeDok/keymap.c
+++ keyboards/sofle_choc/keymaps/CodeDok/keymap.c
@@ -22,17 +22,23 @@
 #define GAME 3
 #define FUNC 4
 
-/* MACROS */
-
 
 /* ----------- MACROS -----------*/
+
+enum tap_dance {
+    TD_END,
+    TD_HOME
+};
 
 enum custom_keycodes {
 	SCREENSHOT = SAFE_RANGE,
 	AMD_FULL,
 	AMD_OVERLAY,
 	STEAM_OVERLAY,
-	DISCORD_OVERLAY
+	DISCORD_OVERLAY,
+	INTELLIJ_MOVE_FORWARD,
+	INTELLIJ_MOVE_BACKWARD,
+    INTELLIJ_SHOW_IN_EXPLORER
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -56,28 +62,57 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 		case AMD_OVERLAY:
 			if (record->event.pressed) {
-				SEND_STRING(SS_LALT("o"));
+				SEND_STRING(SS_DOWN(X_LCTL));
+                SEND_STRING(SS_DOWN(X_LSFT));
+                SEND_STRING(SS_TAP(X_O));
+                SEND_STRING(SS_UP(X_LCTL));
+                SEND_STRING(SS_UP(X_LSFT));
 			} else {}
 			break;
 
 		case STEAM_OVERLAY:
 			if (record->event.pressed) {
-				SEND_STRING(SS_DOWN(X_LCTL));
-                SEND_STRING(SS_DOWN(X_TAB));
-                SEND_STRING(SS_UP(X_LCTL));
-                SEND_STRING(SS_UP(X_TAB));
+				SEND_STRING(SS_DOWN(X_LSFT));
+                SEND_STRING(SS_TAP(X_F1));
+                SEND_STRING(SS_UP(X_LSFT));
 			} else {}
 			break;
 
 		case DISCORD_OVERLAY:
 			if (record->event.pressed) {
-				SEND_STRING(SS_DOWN(X_LGUI));
+				SEND_STRING(SS_DOWN(X_LCTL));
                 SEND_STRING(SS_DOWN(X_LSFT));
                 SEND_STRING(SS_TAP(X_HOME));
-                SEND_STRING(SS_UP(X_LGUI));
+                SEND_STRING(SS_UP(X_LCTL));
                 SEND_STRING(SS_UP(X_LSFT));
 			} else {}
 			break;
+		case INTELLIJ_MOVE_FORWARD:
+			if (record->event.pressed) {
+				SEND_STRING(SS_DOWN(X_LALT));
+				SEND_STRING(SS_DOWN(X_LSFT));
+                SEND_STRING(SS_TAP(X_RIGHT));
+                SEND_STRING(SS_UP(X_LSFT));
+                SEND_STRING(SS_UP(X_LALT));
+			} else {}
+            break;
+        case INTELLIJ_MOVE_BACKWARD:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LALT));
+				SEND_STRING(SS_DOWN(X_LSFT));
+                SEND_STRING(SS_TAP(X_LEFT));
+                SEND_STRING(SS_UP(X_LSFT));
+                SEND_STRING(SS_UP(X_LALT));
+            } else {}
+            break;
+        case INTELLIJ_SHOW_IN_EXPLORER:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LALT));
+                SEND_STRING(SS_TAP(X_1));
+                SEND_STRING(SS_UP(X_LALT));
+                SEND_STRING(SS_TAP(X_1));
+            } else {}
+            break;
     }
 	return true;
 }
@@ -93,8 +128,6 @@ bool caps_word_press_user(uint16_t keycode) {
             return true;
         // Keycodes that continue Caps Word, without shifting.
         case KC_1 ... KC_0:
-		case KC_LSFT:
-		case KC_RSFT:
         case KC_BSPC:
         case KC_DEL:
         case KC_UNDS:
@@ -104,6 +137,11 @@ bool caps_word_press_user(uint16_t keycode) {
             return false;  // Deactivate Caps Word.
     }
 }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, LCTL(KC_HOME)),
+    [TD_END] = ACTION_TAP_DANCE_DOUBLE(KC_END, LCTL(KC_END)),
+};
 
 
 /* ----------- ENCODERS ----------- */
@@ -147,28 +185,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	/*SYMB*/
 	[SYMB] = LAYOUT(
 			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,           KC_NO,         KC_NO,          KC_NO,          KC_NO,
-            KC_NO,          KC_ESC,         KC_NO,              DE_LPRN,        DE_RPRN,        KC_NO,                                          KC_NO,          DE_UDIA,         KC_UP,         DE_ODIA,        DE_EQL,         DE_ACUT,
-			KC_LSFT,        DE_ADIA,        DE_SS,              DE_LCBR,        DE_RCBR,        DE_SLSH,                                        KC_HOME,        KC_LEFT,         KC_DOWN,       KC_RIGHT,       KC_END,         KC_RSFT,
-			KC_LCTL,        DE_CIRC,        DE_PIPE,            DE_LBRC,        DE_RBRC,        DE_BSLS,        KC_NO,          KC_NO,          KC_NO,          DE_PLUS,         KC_BSLS,       DE_EXLM,        DE_QUES,        KC_NO,
-											KC_TRNS,            KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_LGUI,        KC_TRNS,         KC_NO,         KC_TRNS
+            KC_NO,          KC_ESC,         DE_QUOT,            DE_LPRN,        DE_RPRN,        KC_INS,                                         KC_ESC,         DE_UDIA,         KC_UP,         DE_ODIA,        DE_EQL,         DE_ACUT,
+			KC_LSFT,        DE_ADIA,        DE_SS,              DE_LCBR,        DE_RCBR,        DE_SLSH,                                        TD(TD_HOME),    KC_LEFT,         KC_DOWN,       KC_RIGHT,       TD(TD_END),     KC_RSFT,
+			KC_LCTL,        DE_CIRC,        DE_PIPE,            DE_LBRC,        DE_RBRC,        DE_BSLS,        KC_NO,          KC_NO,          KC_NO,          DE_PLUS,         KC_BSLS,       DE_EXLM,        DE_QUES,        KC_RCTL,
+											KC_TRNS,            KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_LALT,        KC_DEL,         KC_TRNS,         KC_NO,         KC_LGUI
 	),
 
 	/* MACRO */
 	[MACRO] = LAYOUT(
-			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-			KC_NO,          KC_NO,          SCREENSHOT,   		KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-											KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
+			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,                      KC_NO,                      KC_NO,                      KC_NO,          KC_NO,
+			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,                      KC_NO,                      KC_NO,                      KC_NO,          KC_NO,
+			KC_NO,          KC_NO,          SCREENSHOT,   		KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          INTELLIJ_MOVE_BACKWARD,     INTELLIJ_SHOW_IN_EXPLORER,  INTELLIJ_MOVE_FORWARD,      KC_NO,          KC_NO,
+			KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                      KC_NO,                      KC_NO,                      KC_NO,          KC_NO,
+											KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                      KC_NO,                      KC_NO
 	),
 
 	/* GAME */
 	[GAME] = LAYOUT(
-			KC_LGUI,        KC_NO,          KC_1,               KC_2,           KC_3,           KC_4,                                           TO(BASE),       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_ESC,
+			KC_LGUI,        KC_NO,          KC_1,               KC_2,           KC_3,           KC_4,                                           KC_MPRV,        KC_MPLY,        KC_MNXT,        KC_NO,          KC_NO,          KC_ESC,
 			KC_F1,          KC_TAB,         KC_Q,               KC_W,           KC_E,           KC_R,                                           DE_Z,           KC_U,           KC_I,           KC_O,           KC_P,           AMD_FULL,
 			KC_F2,          KC_LSFT,        KC_A,               KC_S,           KC_D,           KC_F,                                           KC_H,           KC_J,           KC_K,           KC_L,           STEAM_OVERLAY,  AMD_OVERLAY,
 			KC_F3,          KC_LCTL,        DE_Y,               KC_X,           KC_C,           KC_V,           KC_TRNS,        KC_NO,          KC_N,           KC_M,           KC_NO,          KC_NO,          DISCORD_OVERLAY,KC_NO,
-											KC_5,            	KC_LALT,        KC_B,           KC_SPC,         KC_G,           KC_NO,          MO(FUNC),       KC_NO,          KC_NO,          KC_NO
+											KC_5,            	KC_LALT,        KC_B,           KC_SPC,         KC_G,           KC_ENT,         MO(FUNC),       KC_NO,          KC_NO,          TO(BASE)
 	),
 
 	/* FUNC */
